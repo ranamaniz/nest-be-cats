afd64dd15aed981ee83daad3f9f84a6c3a823761
manish <manish.rana@innovatetech.co>
2026-06-30 17:23:53 +0545

    initiate cats controllers

---
+++ src/cats/dto/index.ts
@@ -0,0 +1,12 @@
+export class CreateCatDto {
+    name: string;
+    age: number;
+    breed: string
+}
+
+
+export class UpdateCatDto extends CreateCatDto { }
+
+export class ListAllEntities {
+    limit: number
+}
